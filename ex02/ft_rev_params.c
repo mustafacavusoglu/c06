88862fe129634248e8942114502cf913f655ef53
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2022-03-01 12:50:59 +0300

    Add files via upload

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcavusog <mcavusog@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 17:28:13 by mcavusog          #+#    #+#             */
+/*   Updated: 2022/02/28 17:28:43 by mcavusog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	n;
+
+	n = 0;
+	while (str[n])
+		write(1, &str[n++], 1);
+	return ;
+}
+
+int	main(int argc, char *argv[])
+{
+	while (argc > 1)
+	{	
+		ft_putstr(argv[--argc]);
+		ft_putstr("\n");
+	}
+	return (0);
+}
